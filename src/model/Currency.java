aa23b1a8def030193a98a58fd83554e9e83dccf7
lednix <ianavdeev43@gmail.com>
2026-03-03 17:03:09 +0500

    Добавление фишки с мультивалютностью с переводом в рубли

--- src/model/Currency.java
+++ src/model/Currency.java
@@ -1,9 +1,15 @@
 package com.fintrack.backend.model;
 
-public enum Currency { // добавил что бы не было путаницы между "USD" "usd" и тд
-    RUB("₽"), USD("$"), EUR("€");
+public enum Currency {
+    RUB("₽"), USD("$"), EUR("€");
 
-    private final String symbol;
-    Currency(String symbol) { this.symbol = symbol; }
-    public String getSymbol() { return symbol; }
+    private final String symbol;
+
+    Currency(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
 }
